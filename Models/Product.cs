f42557176f775779fed1d72078aecc6361ff8a45
rudzzz <rudsolza13@gmail.com>
2023-05-15 21:36:12 -0300

    code

---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CraftsWebApp.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Maker { get; set; }
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int[] Ratings { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Product>(this);
+        }
+    }
+
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using CraftsWebApp.Models;
+using CraftsWebApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CraftsWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
