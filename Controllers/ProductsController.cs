35e49b2366caedd7cd9fd3abfd6b6d3a6b4bebd0
rudzzz <rudsolza13@gmail.com>
2023-05-15 21:35:35 -0300

    code

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using CraftsWebApp.Models;
+using CraftsWebApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CraftsWebApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;   
+        }
+
+        public JsonFileProductService ProductService { get; set; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        [HttpGet]
+        [Route("Rate")]
+        public ActionResult Get([FromQuery] string productId, [FromQuery] int rating)
+        {
+            ProductService.AddRating(productId, rating);
+            return Ok();
+        }
+    }
+}
